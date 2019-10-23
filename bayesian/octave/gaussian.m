806f384e7d4505d0ed8bbb9a3933eed19fc7d6b8
Arthur Caetano Sabino Santos <arthur.santos@gjccorp.com.br>
2019-10-23 20:03:26 -0300

    Prearando pro relatório

---
+++ bayesian/octave/gaussian.m
@@ -0,0 +1,68 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                             Bayesian - Arthur Santos                         %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clear all;close all;
+%% Read Dataset
+file = "../../data/iris_rand.csv"
+
+data = readdataset(file, 5);
+x = zeros(size(data));
+x(:,end) = cell2mat(lable_encode(data(:,end)));
+var_labels = {"SepalLengthCm","SepalWidthCm","PetalLengthCm","PetalWidthCm"};
+
+for i=1:size(data)(2)-1
+  x(:,i) = str2double(data(:,i));
+end
+
+%% Split train and test
+slice = floor(length(x)*0.8) 
+y_train = x(1:slice,end);
+x_train = x(1:slice,1:end-1)';
+
+y_test = x(slice+1:end,end);
+x_test = x(slice+1:end,1:end-1)';
+
+for n=1:size(x_train)(1)
+  figure(n)
+  b = min(x_train(n,:));
+  a = max(x_train(n,:));
+  b = [b:(a-b)/10:a];
+  subplot(311)
+  hist(x_train(n,y_train==1), b)
+  ylabel("Numero de Iris-setosa")
+  title(["Histograma ", var_labels{n}])
+  subplot(312)
+  hist(x_train(n,y_train==2), b)
+  ylabel("Numero de Iris-versicolor")
+  subplot(313)
+  hist(x_train(n,y_train==3), b)
+  ylabel("Numero de Iris-virginica")
+  xlabel(["valor de ", var_labels{n}])
+end
+
+%% Estimate and Predict
+model = estimator(x_train,y_train);
+err = zeros(length(y_train),1);
+ypred = predict(model, x_train);
+
+%% Error Calcmodel.R
+err = ypred!=y_train;
+
+%% Results
+printf ("Erro de %d em %d: %d%%\n", sum(err), length(y_train),
+ 100*(sum(err)/length(y_test)));
+printf ("Acuracia de %d em %d: %d%%\n", length(y_train)-sum(err),
+ length(y_train), 100*((length(y_train)-sum(err))/length(y_train)));
+
+%% Error Calcmodel.R
+err = zeros(length(x_test),1);
+ypred = predict(model, x_test);
+
+%% Error Calcmodel.R
+err = ypred!=y_test;
+
+%% Results
+printf ("Erro de %d em %d: %d%%\n", sum(err), length(y_test),
+ 100*(sum(err)/length(y_test)));
+printf ("Acuracia de %d em %d: %d%%\n", length(y_test)-sum(err),
+ length(y_test), 100*((length(y_test)-sum(err))/length(y_test)));
---
+++ bayesian/octave/naive_bayes2.m
@@ -0,0 +1,51 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                         Naive Bayesian - Arthur Santos                       %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clear all;
+
+function f = text_filter(a, t)
+  f = cellfun(@(x) isequal(x, t ), a);
+endfunction
+
+%% Read Dataset
+file = "../../data/iris_rand_cat.csv";
+x = readdataset(file,5);
+
+slice = floor(length(x)*0.8)
+y_train = x(1:slice,end);
+x_train = x(1:slice,1:end-1);
+
+y_test = x(slice+1:end,end);
+x_test = x(slice+1:end,1:end-1);
+
+err = zeros(length(x_test),1);
+ypred = zeros(length(x_test),1);
+
+%% Predict
+for k = 1:length(x_test)
+  c = unique(y_train);
+  p = zeros(1,length(c));
+
+  for i=1:length(c)
+    p(i) = sum(text_filter(y_train,c{i}))/length(y_train);
+
+    for j=1:size(x_train)(2)
+      p(i) = p(i) * sum(text_filter(x_train(text_filter(y_train,c{i}), j),x_test{k,j}))/sum(text_filter(y_train,c{i}));
+    endfor
+    
+  endfor
+  if(sum(p)==0)
+    k
+    x_test(k)
+  endif
+  
+  p = p/sum(p);
+  
+  [v,index] = max(p);
+  ypred(k) = index;
+  err(k) = ~isequal(y_test{k}, c{index});
+endfor
+
+%% Results
+printf ("Erro de %d em %d: %d%%\n", sum(err), length(y_test), 100*(sum(err)/length(y_test)));
+printf ("Acuracia de %d em %d: %d%%\n", length(y_test)-sum(err), length(y_test), 100*((length(y_test)-sum(err))/length(y_test)));
